af67296ae571f27f04c8a231d41b4e3a056590df
odettechauncy <odette.chauncy@dairynz.co.nz>
2021-07-15 13:23:06 +1200

    demo script - added notes

--- Demo script.R
+++ Demo script.R
@@ -2,3 +2,11 @@
 
 usethis::use_git()
 
+#Lets aswsume one folder on my computer euqals one R studio file which equale one Github repo.
+#to get any project into a git hub rep 
+# usethis:: use_git() # connects project to git on your computer 
+# usethis::use_github() #connects git to github
+## Important options to consider for use_github()
+#1. Organisation = "DairyNZ"
+#2. Private = TRUE #default is FALSE, i.e ut will be public!!
+
